4d096dda68dfa0808b04712875c69cb0732b446f
Peter Delgado <peterrafaeldelgado@gmail.com>
2022-10-18 11:32:34 +0200

    Initial Commit

---
+++ LinkedList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LinkedList
+//
+//  Created by Peter Delgado on 18/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LinkedList/LinkedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedListApp.swift
+//  LinkedList
+//
+//  Created by Peter Delgado on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LinkedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
